b26d5ac8f99f09b8b754cfff44ac6222594d4891
cotecharles92 <cotecharles92@gmail.com>
2013-09-17 17:16:16 -0400

    rename ex_02 to have an extention

---
+++ bin/ex_02.dart
@@ -0,0 +1,36 @@
+import 'dart:math';
+
+ex02_1() {
+  num x = 5;
+  num y = 7;
+  num z = 8;
+  print(x-y/z);
+}
+
+
+ex02_2() {
+  num r = 10e2;
+  print(r*PI);
+}
+
+
+ex02_3() {
+  num a = 220;
+  num b = 260;
+  num c = 0.00002295684114092842;
+  print(a*b*c);
+}
+
+
+ex02_4() {
+  num m = 100;
+  num s = 9.58;
+  print(m/s);
+}
+
+main() {
+  ex02_1();
+  ex02_2();
+  ex02_3();
+  ex02_4();
+}
